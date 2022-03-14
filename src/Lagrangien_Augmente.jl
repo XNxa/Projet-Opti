854affd671dc2c0ae5496bf4710fbe48ed3589a0
sophie-mauran <60010583+sophie-mauran@users.noreply.github.com>
2022-03-14 10:42:00 +0100

    suppression du paramètre epsilon inutile, différenciation de tol en tol_rel et tol_abs, ajout d'un commentaire pour les tolérances des algos appelés dans Lagrangien Augmenté

--- src/Lagrangien_Augmente.jl
+++ src/Lagrangien_Augmente.jl
@@ -53,24 +53,30 @@ grad_contrainte(x) = [2*x[1] ;2*x[2]]
 hess_contrainte(x) = [2 0;0 2]
 output = Lagrangien_Augmente(algo,f,contrainte,gradf,hessf,grad_contrainte,hess_contrainte,x0,options)
 ```
+
+# Tolérances des algorithmes appelés
+
+Pour les tolérances définies dans les algorithmes appelés (Newton et régions de confiance), prendre les tolérances 
+par défaut définies dans ces algorithmes.
+
 """
 function Lagrangien_Augmente(algo,fonc::Function,contrainte::Function,gradfonc::Function,
 	hessfonc::Function,grad_contrainte::Function,hess_contrainte::Function,x0,options)
 
 	if options == []
-		epsilon = 1e-8
-		tol = 1e-5
+		tol_rel = 1e-5
+    tol_abs = 1e-7
 		itermax = 1000
 		lambda0 = 2
 		mu0 = 100
 		tho = 2
 	else
-		epsilon = options[1]
-		tol = options[2]
-		itermax = options[3]
-		lambda0 = options[4]
-		mu0 = options[5]
-		tho = options[6]
+		tol_rel = options[2]
+    tol_abs = options[3]
+		itermax = options[4]
+		lambda0 = options[5]
+		mu0 = options[6]
+		tho = options[7]
 	end
 
   n = length(x0)
